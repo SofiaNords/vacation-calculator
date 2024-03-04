3b27e88c18dbf6b559f6540f732b5fe5a0ff97ed
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-03-04 07:30:10 +0000

    Import datetime module and create validate vacation year function

--- run.py
+++ run.py
@@ -1,13 +1,31 @@
+import datetime
+
+
 def get_vacation_year():
     """
     The user selects which vacation year they want to calculate
     """
+    while True:
+        global vacation_year
+        vacation_year =
+        input("Enter the vacation year you want to calculate (YYYY):")
 
-    global vacation_year
-    vacation_year =
-    input("Enter the vacation year you want to calculate (YYYY):")
+        if validate_vacation_year(vacation_year):
+            print(f"Thank you, your vacation year is {vacation_year}.")
+            break
 
-    print(f"Thank you, your vacation year is {vacation_year}.")
+
+def validate_vacation_year(vacation_year):
+    """
+    Validates vacation year date format.
+    """
+    try:
+        vacation_year = datetime.datetime.strptime(vacation_year, "%Y")
+    except ValueError:
+        print("Incrorrect employment date format, please try again.")
+        return False
+
+    return True
 
 
 def main():

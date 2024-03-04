01571dbb073190d3fe140061a4c3164e08eec996
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-03-04 07:17:48 +0000

    Add get vacation year function and main function to run all program functions

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_vacation_year():
+    """
+    The user selects which vacation year they want to calculate
+    """
+
+    global vacation_year
+    vacation_year =
+    input("Enter the vacation year you want to calculate (YYYY):")
+
+    print(f"Thank you, your vacation year is {vacation_year}.")
+
+
+def main():
+    """
+    Run all program functions
+    """
+    get_vacation_year()
+
+
+main()
